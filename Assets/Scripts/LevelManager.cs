4d58bc35b5765528d9be97d36eb539ef273ec094
Tristanchan2020 <616735621@qq.com>
2022-03-28 14:48:39 +0800

    Level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,7 +7,7 @@ public class LevelManager : MonoBehaviour
     Rigidbody2D rb;
     public float Speed;
     Transform recyclePoint;
-    Transform startTransform;
+    Vector3 startTransform;
     float instantiateTriggerValue;
     public Transform InstantiateLine;
     void Start()
@@ -16,13 +16,13 @@ public class LevelManager : MonoBehaviour
         rb.velocity = Vector2.down * Speed;
         recyclePoint = this.transform.GetChild(0).transform;
         instantiateTriggerValue = InstantiateLine.position.y;
-        startTransform = this.transform;
+        startTransform = this.transform.position;
     }
     private void Update()
     {
         if (recyclePoint.position.y<instantiateTriggerValue)
         {
-            this.transform.position = startTransform.position;
+            this.transform.position = startTransform;
         }
     }
 }
